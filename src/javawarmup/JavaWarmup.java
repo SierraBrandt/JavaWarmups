dbdcbc017fa1c9d03ab3b7a94079e385fb82d94d
SierraBrandt <sierrabrandt1@gmail.com>
2020-03-01 12:06:12 -0800

    First Commit

---
+++ src/javawarmup/JavaWarmup.java
@@ -0,0 +1,24 @@
+
+package javawarmup;
+
+
+public class JavaWarmup {
+    
+    public static void main(String[] args) {
+        
+    }
+//prints str, n times
+//Febuary 29 2020
+    public static void warmup1() {
+        int n = 2;
+        String str = "Hi";
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result += str;
+        }
+        System.out.println (result);
+    }
+
+
+
+}
