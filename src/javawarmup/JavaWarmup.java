3af631ad1e016db948007a1958ef30f71b3c6bfe
SierraBrandt <sierrabrandt1@gmail.com>
2020-03-01 12:10:12 -0800

    Second Commit

--- src/javawarmup/JavaWarmup.java
+++ src/javawarmup/JavaWarmup.java
@@ -5,8 +5,10 @@ package javawarmup;
 public class JavaWarmup {
     
     public static void main(String[] args) {
-        
+        warmup1();
+        warmup2();
     }
+    
 //prints str, n times
 //Febuary 29 2020
     public static void warmup1() {
@@ -18,7 +20,17 @@ public class JavaWarmup {
         }
         System.out.println (result);
     }
-
+    
+//adds up all values in an array
+//March 1 2020
+    public static void warmup2() {
+        int array[]={-3, 5, 6, 10};
+        int result = 0;
+        for(int i=0; i < array.length; i++){
+             result+=array[i];
+        }
+        System.out.println(result);
+    }
 
 
 }
